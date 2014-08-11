94a3f04df69dcddfe8d2c8ab076d0508c5d0715c
Ronald Partridge <rond@ronald-douglas.com>
2014-08-10 20:02:30 -0400

    Layout updates to test file

--- simba-ad-engine.js
+++ simba-ad-engine.js
@@ -153,6 +153,97 @@ simbaAdEngine = function($, _) {
         $(cycleButtons).click(cycleAdblock);
     }
 
+    var renderLeaderBox = function(element, merchantList, adBlockCollection) {
+
+        var adBlock = adBlockCollection[1];
+
+        var m = findMerchant(merchantList, adBlock.merchantId);
+
+        var bodyImg = _.template('<div class="productContainer"><img class="simbaAdImage" src="<%= src %>" alt="<%= alt %>" /></div>',
+            {
+                src: adBlock.imageURL,
+                alt: adBlock.name
+            });
+
+        var metaData = _.template('<span class="brand"><%= merchant %></span>' +
+            '<span class="description"><%= description %></span>' +
+            '<span class="price">$<%= price %></span><span class="priceTag">&nbsp;</span><span class="link">Hurry &amp; Save!</span>',
+            {
+                merchant: m.name,
+                description: adBlock.name,
+                price: adBlock.salePrice
+            });
+
+
+        var template = _.template(
+            '<div class="simbaBigBox">' +
+                '<div class="smHeader"></div>' +
+                '<div class="smBody">' +
+                '<div class="smArrow"></div>' +
+                '<%= bodyImg %>' +
+                '<div class="meta"><div class="meta-inner"><%= metaData %></div>' +
+                '<div class="nav" simba-position="1"><img src="/adgroups/bigbox/prev-btn.png" class="prev" /><img src="/adgroups/bigbox/next-btn.png" class="next" /></div>' +
+                '</div></div>' +
+                '<div class="smFooter"></div>', { bodyImg: bodyImg, metaData: metaData });
+
+
+        $(element).attr('data-url',adBlock.deepLink);
+        $(element).append(template, null);
+        $(element).find('.productContainer, .meta-inner').click(function() {
+
+            var productUrl = $(element).attr('data-url');
+            location.href = productUrl;
+        });
+
+
+        var cycleButtons = $(element).children().find('.prev, .next');
+
+        var cycleAdblock = function() {
+
+            var nav = $(this).parent();
+
+            var clickPosition = parseInt($(nav).attr('simba-position'));
+
+            if($(this).hasClass('next')) {
+
+                clickPosition++;
+
+                if(clickPosition == adBlockCollection.length) {
+                    clickPosition = 0;
+                }
+            }
+
+            if($(this).hasClass('prev')) {
+
+                clickPosition--;
+
+                if(clickPosition == -1) {
+                    clickPosition = adBlockCollection.length - 1;
+                }
+            }
+
+            var adBlock = adBlockCollection[clickPosition];
+            var adBlockElement = $(this).parent().parent();
+
+            var img = $(adBlockElement).parent().find('.simbaAdImage')[0];
+
+            $(img).attr('src', adBlock.imageURL);
+            $(img).attr('alt', adBlock.name);
+
+
+            $($(adBlockElement).find('.brand')[0]).text(m.name);
+            $($(adBlockElement).find('.description')[0]).text(adBlock.name);
+            $($(adBlockElement).find('.price')[0]).text('$' + adBlock.salePrice);
+
+            $(adBlockElement).parent().parent().parent().attr('data-url', adBlock.deepLink);
+
+            $(nav).attr('simba-position', clickPosition);
+
+        }
+
+        $(cycleButtons).click(cycleAdblock);
+    }
+
     function refresh() {
 
         $('div[simba-deals]').each(function() {
@@ -175,7 +266,7 @@ simbaAdEngine = function($, _) {
 
                     if(bannerType == 'LEADER_BOX') { // Assume bigbox is true filler logic
 
-                        renderBanner(element, merchantList, adBlockCollection);
+                        renderLeaderBox(element, merchantList, adBlockCollection);
                     }
                 }
             });
